b72bf6485f5ba3969226d638407e984e70dfa9d3
Hardik Srivastava <hardiksrivastava959@gmail.com>
2022-01-27 12:20:18 +0530

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Home(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Times",
+              style: TextStyle(color: Colors.black),
+            ),
+            Text(
+              "News",
+              style: TextStyle(color: Colors.lightBlue),
+            ),
+          ],
+        ),
+        elevation: 0.0,
+      ),
+    );
+  }
+}
